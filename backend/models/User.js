a52bc258c2e13819de7145cd2ba3d963e9f2228a
Rija <learn.rija@gmail.com>
2025-12-21 21:54:40 +0545

    BACKEND:- created order, product, user models

---
+++ backend/models/User.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrpt.js';
+
+const userSchema = mongoose.Schema({
+    name : {type: String, required: true},
+    email : {type: String, required: true, unique: true},
+    password : {type: String, required: true},
+    isAdmin : {type: Boolean, required: true, default: false }
+}, {
+    timestamps: true,
+});
+
+// function to check users entered pass with their saved pass
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return enteredPassword === this.password;
+};
+
+const User = mongoose.model('User', userSchema);
+export default User;
